77188e525a800bc69d40e50b06c5817302e7fa6f
megatemik <103453323+artemzemin@users.noreply.github.com>
2025-11-02 01:32:54 -0700

    Add files via upload

---
+++ lab1_infa.ino
@@ -0,0 +1,21 @@
+program lab1;
+
+    var
+        a,b,x,z:real;
+    begin
+        writeLn('Введите A: ');
+        readLn(A);
+        writeLn('Введите B: ');
+        readLn(B);
+        
+        x:=sqrt(abs(a) + sqr(a+b) / a * b);
+        
+        z:= b * (sin(x)/cos(x) + exp(x));
+        
+        
+        writeLn('Вывод результатов:');
+        writeLn('z=', z:0:4);
+    end.
+    
+
+    //лабараторная по информатике номер 1 вариант 4 
