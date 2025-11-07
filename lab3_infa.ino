9772554b11d2dcb5e2f6ec64f95d0690aec05c4a
megatemik <103453323+artemzemin@users.noreply.github.com>
2025-11-07 06:28:42 -0800

    Add files via upload

---
+++ lab3_infa.ino
@@ -0,0 +1,16 @@
+program lab3;
+    var 
+        x,y:real;
+        n:integer;
+        
+    begin
+        writeln('Введите:');
+        n=2;
+        x:=1;
+        while x <=3 do 
+    begin
+    y:=sin(n*x)-cos(n/x);
+    writeln('x='x:4:2, 'y='y:4:2);
+    x:=x+0,1;
+        end;
+    end.
